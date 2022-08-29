bb2cf34f8e5c780bbfb4449ab39856c1afa626d9
Thiệu Trung Kiên <87187870+TrunqKj3n@users.noreply.github.com>
2022-08-29 10:58:48 +0700

    Update index.js

--- index.js
+++ index.js
@@ -12,14 +12,12 @@ var axios = require("axios");
 console.clear(), console.log(chalk.green("[ BOT ] : ") + chalk.white("Bot đã khởi động!")), console.log(chalk.green("[ AUTHOR ] : ") + chalk.white("Thiệu Trung Kiên")), console.log(chalk.green("[ EMAIL ] : ") + chalk.white("ttk.trungkien333@gmail.com")), console.log(chalk.green("[ GITHUB ] : ") + chalk.white("TrunqKj3n")), console.log(chalk.green("[ TELEGRAM ] : ") + chalk.white("@thieutrungkien")), console.log(chalk.green("[ FACEBOOK ] : ") + chalk.white("https://www.facebook.com/ThieuTrungKien.Profile")), console.log(chalk.white("-----------------------------------------------------\n"));
 const app = express();
 bot.onText(/\/start/, (msg => {
-    console.log(chalk.white("[ MODULE ] : ") + chalk.green(`${msg.from.first_name} ${msg.from.last_name}`) + chalk.yellow(` ${msg.text}`)), bot.sendMessage(msg.chat.id, "Chào mừng bạn đến với Kaguya!\n\nĐể sử dụng Kaguya, hãy gõ vào câu lệnh bên dưới:\n/help - Hiển thị danh sách các câu lệnh có sẵn\n/echo - Lặp lại tin nhắn của bạn!")
-})),
-app.use(cors());
-
+        console.log(chalk.white("[ MODULE ] : ") + chalk.green(`${msg.from.first_name} ${msg.from.last_name}`) + chalk.yellow(` ${msg.text}`)), bot.sendMessage(msg.chat.id, "Chào mừng bạn đến với Kaguya!\n\nĐể sử dụng Kaguya, hãy gõ vào câu lệnh bên dưới:\n/help - Hiển thị danh sách các câu lệnh có sẵn\n/echo - Lặp lại tin nhắn của bạn!")
+    })),
+    app.use(cors());
 const port = process.env.PORT || 3000;
-
 app.listen(port, (() => console.log(`Listening on port ${port}`))),
-app.get("/", ((req, res) => res.send("Hello World!"))),
-bot.getMe().then((data => {
-    console.log("Đăng nhập thành công với tài khoản bot: " + data.username)
-}));
+    app.get("/", ((req, res) => res.send("Hello World!"))),
+    bot.getMe().then((data => {
+        console.log("Đăng nhập thành công với tài khoản bot: " + data.username)
+    }));
